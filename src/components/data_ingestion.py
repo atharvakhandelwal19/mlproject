19131f0c4ac98fb68936bc81fe434cd078e056fc
atharvakhandelwal19 <atharvakhandelwal19@gmail.com>
2026-01-28 08:51:34 +0530

    Model Training Pipeline Completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_training import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,4 +52,7 @@ if __name__=="__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_training(train_arr, test_arr))
